da2f20caca5f14a8f09b27abf2cd12855933d5c0
Richard Chase Mooney <rchasemooney@gmail.com>
2025-09-30 16:39:03 -0400

    Add code coverage for C and D grades and GradeType.String()

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -34,6 +34,64 @@ func TestGetGradeB(t *testing.T) {
 	}
 }
 
+func TestGetGradeC(t *testing.T) {
+	expected_value := "C"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 100, Assignment)
+	gradeCalculator.AddGrade("exam 1", 35, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 52, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeD(t *testing.T) {
+	expected_value := "D"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 82, Assignment)
+	gradeCalculator.AddGrade("exam 1", 42, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 67, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGradeTypeToString(t *testing.T) {
+	expected_value1 := "assignment"
+
+	actual_value1 := Assignment.String()
+
+	if expected_value1 != actual_value1 {
+		t.Errorf("Expected GradeType.String() to return '%s'; got '%s' instead", expected_value1, actual_value1)
+	}
+
+	expected_value2 := "exam"
+
+	actual_value2 := Exam.String()
+
+	if expected_value2 != actual_value2 {
+		t.Errorf("Expected GradeType.String() to return '%s'; got '%s' instead", expected_value2, actual_value2)
+	}
+
+	expected_value3 := "essay"
+
+	actual_value3 := Essay.String()
+
+	if expected_value3 != actual_value3 {
+		t.Errorf("Expected GradeType.String() to return '%s'; got '%s' instead", expected_value3, actual_value3)
+	}
+}
+
 func TestGetGradeF(t *testing.T) {
 	expected_value := "F"
 
